05c8e5ec2f194b7007dc2402f6d73f2f7d6a605b
besteffects <besteffects@gmail.com>
2019-10-23 20:07:21 -0400

    switch with the help of String

--- src/Strings.java
+++ src/Strings.java
@@ -1,11 +1,37 @@
 public class Strings {
     public static void main(String[] args) {
+        substringCreate();
+        stringSwitch();
+
+    }
+
+    public static void substringCreate() {
         String orgstr = "Java makes the web move.";
 
         //Construct a substring
-        String substr= orgstr.substring(5,18);
+        String substr = orgstr.substring(5, 18);
 
         System.out.println("orgstr: " + orgstr);
         System.out.println("substr: " + substr);
+
+    }
+
+    public static void stringSwitch() {
+        String command = "cancel";
+        switch (command) {
+            case "connect":
+                System.out.println("Connecting");
+                break;
+            case "cancel":
+                System.out.println("Cancelling");
+                break;
+            case "disconnect":
+                System.out.println("Disconnecting");
+                break;
+            default:
+                System.out.println("Command Error!");
+                break;
+        }
+
     }
 }
