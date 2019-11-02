d066699abe2bcaf445583fa6167eb548629d69fe
besteffects <besteffects@gmail.com>
2019-11-02 02:41:27 -0400

    Inheratance example 2

--- src/inheritance/TwoDShape.java
+++ src/inheritance/TwoDShape.java
@@ -2,37 +2,57 @@ package inheritance;
 
 class TwoDShape {
     double width;
-     double height;
+    double height;
 
-     void showDim(){
-         System.out.println("Width and height are: " + width + " and " + height);
-     }
+    void showDim() {
+        System.out.println("Width and height are: " + width + " and " + height);
+    }
 }
 
 //A subclass of TwoDshape for triangles
 
-class Triangle extends TwoDShape{
+class Triangle extends TwoDShape {
     String style;
+
     double area() {
-        return width*height/2;
+        return width * height / 2;
     }
 
-    void showStyle(){
+    void showStyle() {
         System.out.println("Triangle is " + style);
     }
 }
-class Shapes{
+
+class Rectangle extends TwoDShape {
+    boolean isSquare() {
+        if (width == height) {
+            return true;
+        }
+        return false;
+    }
+
+    double area() {
+        return width * height;
+    }
+}
+
+class Shapes {
     public static void main(String[] args) {
         Triangle t1 = new Triangle();
         Triangle t2 = new Triangle();
+        Rectangle r1 = new Rectangle();
+        r1.width = 5.3;
+        r1.height = 5.3;
+        if (r1.isSquare()) {
+            System.out.println("r1 area: " + r1.area());
+        }
+        t1.width = 4.0;
+        t1.height = 4.0;
+        t1.style = "filled";
 
-        t1.width=4.0;
-        t1.height= 4.0;
-        t1.style="filled";
-
-        t2.width= 8.0;
-        t2.height=12.0;
-        t2.style="outlined";
+        t2.width = 8.0;
+        t2.height = 12.0;
+        t2.style = "outlined";
 
         System.out.println("Info for t1: ");
         t1.showStyle();
