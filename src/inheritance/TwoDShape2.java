8c9df4220a5938e4aefa2a4d85970e378566d007
besteffects <besteffects@gmail.com>
2019-11-03 01:11:34 -0500

    getters and setters. Constructor in subclass

---
+++ src/inheritance/TwoDShape2.java
@@ -0,0 +1,66 @@
+package inheritance;
+
+class TwoDShape2 {
+    private double width; //these are
+    private double height; // now private
+
+    //accessor methods for width and height
+    double getWidth() {
+        return width;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    void setWidth(double w) {
+        width = w;
+    }
+
+    void setHeight(double h) {
+        height = h;
+    }
+
+    void showDim() {
+        System.out.println("Width and height are " + width + " and " + height);
+    }
+}
+
+// a subclass of TwoDShape2 for triangles.
+class Triangle2 extends TwoDShape2 {
+    private String style;
+
+    //Constructor
+    Triangle2(String s, double w, double h) {
+        setWidth(w);
+        setHeight(h);
+        style = s;
+    }
+
+    double area() {
+        return getWidth() * getHeight() / 2;
+    }
+
+    void showStyle() {
+        System.out.println("Triangle is " + style);
+    }
+}
+
+class Shapes3 {
+    public static void main(String[] args) {
+        Triangle2 t1 = new Triangle2("filled", 4.0, 4.0);
+        Triangle2 t2 = new Triangle2("outlined", 8.0, 12.0);
+
+        System.out.println("Info for t1: ");
+        t1.showStyle();
+        t1.showDim();
+        System.out.println("Area is " + t1.area());
+
+        System.out.println();
+
+        System.out.println("Info for t2: ");
+        t2.showStyle();
+        t2.showDim();
+        System.out.println("Area is " + t2.area());
+    }
+}
