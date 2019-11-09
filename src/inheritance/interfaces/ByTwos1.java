7f696ac2856a562df5e4a1cbd9bf612c4139d708
besteffects <besteffects@gmail.com>
2019-11-09 16:52:19 -0500

    interface implementation and defining own methods

---
+++ src/inheritance/interfaces/ByTwos1.java
@@ -0,0 +1,55 @@
+package inheritance.interfaces;
+//Implement Series and add getPrevious
+
+class ByTwos1 implements Series {
+    int start;
+    int val;
+    int prev;
+
+    ByTwos1() {
+        start = 0;
+        val = 0;
+        prev = -2;
+    }
+
+    public int getNext() {
+        prev = val;
+        val += 2;
+        return val;
+    }
+
+    public void reset() {
+        val = start;
+        prev = start - 2;
+    }
+
+    public void setStart(int x) {
+        start = x;
+        val = x;
+        prev = x - 2;
+    }
+
+    int getPrev() {
+        return prev;
+    }
+}
+
+class SeriesDemo1 {
+    public static void main(String[] args) {
+        ByTwos1 ob = new ByTwos1();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Next value is " + ob.getNext());
+        }
+        System.out.println("\nResetting");
+        ob.reset();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Next value is " + ob.getNext());
+        }
+        System.out.println("\nStarting at 100");
+        ob.setStart(100);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Next value is " + ob.getNext());
+        }
+    }
+
+}
