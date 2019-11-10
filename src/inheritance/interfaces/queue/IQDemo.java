7d479be58ddf6dac96dd7b3ccf74dac43bd2a62b
besteffects <besteffects@gmail.com>
2019-11-10 01:33:16 -0500

    Added DynQueue that expands its size when space is exhausted

--- src/inheritance/interfaces/queue/IQDemo.java
+++ src/inheritance/interfaces/queue/IQDemo.java
@@ -77,3 +77,38 @@ class CircularQueue implements ICharQ {
         return ch;
     }
 }
+
+// A dynamic queue
+class DynQueue implements ICharQ {
+    private char q[]; //this array holds the queue
+    private int putloc, getLoc; // the put and get indices
+
+    //Construct an empty queue given its size
+    public DynQueue(int size) {
+        q = new char[size]; //allocate memory for queue
+        putloc = getLoc = 0;
+    }
+
+    //put the character into the queue
+    public void put(char ch) {
+        if (putloc == q.length) {
+            //increase queue size
+            char t[] = new char[q.length * 2];
+
+            //copy elements into new queue
+            for (int i = 0; i < q.length; i++)
+                t[i] = q[i];
+            q = t;
+        }
+        q[putloc++] = ch;
+    }
+
+    //get a character from the queue.
+    public char get() {
+        if (getLoc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+        return q[getLoc++];
+    }
+}
