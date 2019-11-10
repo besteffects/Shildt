fd9de9759a1947fbfccbecac8c39772ad0beb946
besteffects <besteffects@gmail.com>
2019-11-09 21:16:47 -0500

    Added CircularQueue implementation

--- src/inheritance/interfaces/queue/IQDemo.java
+++ src/inheritance/interfaces/queue/IQDemo.java
@@ -2,7 +2,6 @@ package inheritance.interfaces.queue;
 
 class IQDemo {
 
-
 }
 
 // a fixed-size queue class for characters
@@ -34,5 +33,47 @@ class FixedQueue implements ICharQ {
             return (char) 0;
         }
         return q[getloc++];
+    }
+}
+
+// A circular queue.
+class CircularQueue implements ICharQ {
+    private char q[]; //this array holds the queue
+    private int putloc, getloc; // the put and the get indices
+
+    //Construct an empty queue given its size.
+    public CircularQueue(int size) {
+        q = new char[size + 1]; // allocate memory for queue
+        putloc = getloc = 0;
+    }
+
+    //Put a character into the queue.
+    public void put(char ch) {
+        /*
+        Queue is full if either putloc is one less than getloc, or if putloc is at the end
+        of the array and getloc is at the beginning.
+         */
+        if (putloc + 1 == getloc |
+                ((putloc == q.length - 1) & (getloc == 0))) {
+            System.out.println(" - Queue is full");
+            return;
+        }
+        q[putloc++] = ch;
+        if (putloc == q.length) {
+            putloc = 0;  // loop back
+        }
+    }
+
+    //Get a character from the queue.
+    public char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+        char ch = q[getloc++];
+        if (getloc == q.length) {
+            getloc = 0; // loop back
+        }
+        return ch;
     }
 }
