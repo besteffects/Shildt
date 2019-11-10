6a1f332b737b7f8ba0724572f8692bfbf87c96d6
besteffects <besteffects@gmail.com>
2019-11-09 20:17:05 -0500

    Added FixedQueue implementation

--- src/inheritance/interfaces/Series.java
+++ src/inheritance/interfaces/Series.java
@@ -2,6 +2,8 @@ package inheritance.interfaces;
 
 public interface Series {
     int getNext(); //return next number in series
+
     void reset(); //restart
+
     void setStart(int x); // set starting value
 }
