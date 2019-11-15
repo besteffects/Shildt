97dfa07f3150d97698bd2f738cf03bc3017335a5
besteffects <besteffects@gmail.com>
2019-11-14 20:57:35 -0500

    Reading console input

---
+++ src/IO/ReadBytes.java
@@ -0,0 +1,16 @@
+//Read an array of bytes from the keyboard (reading console input)
+package IO;
+
+import java.io.IOException;
+
+class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        byte data[] = new byte[10];
+        System.out.println("Enter some characters");
+        System.in.read(data); //Enter an array of bytes from the keyboard
+        System.out.print("You entered: ");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println((char) data[i]);
+        }
+    }
+}
