127646d44f23ff3047660435d919a91da90a9956
besteffects <besteffects@gmail.com>
2019-11-15 19:05:50 -0500

    Copying one file to the other with FileInputStream,FileOutputStream

---
+++ src/IO/bytes/CopyFile.java
@@ -0,0 +1,52 @@
+package IO.bytes;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+Copy a text file
+To use this program, specify the name
+of the source fileand the destination file.
+For example, to copy a file called first.txt to second.txt, use the
+following command line.
+  java IO.bytes.CopyFile first.txt second.txt
+    files must be put into the main directory
+ */
+public class CopyFile {
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        //First, make sure that both files has been specified.
+        if (args.length != 2) {
+            System.out.println("Usage: CopyFile from to");
+            return;
+        }
+        // Copy a file
+        try {
+            //Attempt to open the files.
+            fin = new FileInputStream(args[0]);
+            fout = new FileOutputStream(args[1]);
+
+            do {
+                i = fin.read();
+                if (i != -1) fout.write(i);
+            } while (i != -1);
+        } catch (IOException exc) {
+            System.out.println("I/O Error " + exc);
+        } finally {
+            try {
+                if (fin != null) fin.close();
+            } catch (IOException exc) {
+                System.out.println("Error cloning input file");
+            }
+            try {
+                if (fout != null) fout.close();
+            } catch (IOException exc) {
+                System.out.println("Error closing output file");
+            }
+        }
+    }
+}
