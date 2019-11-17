0060ae9dc7786fb27367ae1aaa06d82270bfdf5e
besteffects <besteffects@gmail.com>
2019-11-17 18:58:12 -0500

    Using PrintWriter to handle console output

---
+++ src/IO/chars/PrintWriterDemo.java
@@ -0,0 +1,18 @@
+package IO.chars;
+//Demonstrate PrintWriterDemo (handles console output)
+//Should be used instead sout for internationalization
+
+import java.io.PrintWriter;
+
+class PrintWriterDemo {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true); // create a PrintWriter linked to System.out
+        int i = 10;
+        double d = 123.65;
+        pw.println("Using a PrintWriter");
+        pw.println(i);
+        pw.println(d);
+
+        pw.println(i + " " + d + " is " + (i + d));
+    }
+}
