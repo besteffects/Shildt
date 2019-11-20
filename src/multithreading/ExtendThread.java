a2cc1df9c237c51dea0f410e34bd12278469679a
besteffects <besteffects@gmail.com>
2019-11-19 23:01:54 -0500

    Creating own thread with extending Thread class

--- src/multithreading/ExtendThread.java
+++ src/multithreading/ExtendThread.java
@@ -1,26 +1,64 @@
 package multithreading;
 
 class ExtendThread {
-    class MyThread extends Thread {
-        // Construct a new thread.
-        MyThread(String name) {
-            //name thread. Super is used to call this version of Thread's constructor:
-            //Thread(String threadName)
-            super(name);
-        }
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
 
-        // Entry point of Thread
-        public void run() {
-            System.out.println(getName() + " starting.");
+        MyExtendThread mt = new MyExtendThread("Child #1");
+
+        mt.start();
+
+        //Can be replaced with createAndStart()
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println(".");
             try {
-                for (int count = 0; count < 10; count++) {
-                    Thread.sleep(400);
-                    System.out.println("In " + getName() + ", count is " + count);
-                }
+                Thread.sleep(100);
             } catch (InterruptedException exc) {
-                System.out.println(getName() + " interrupted");
+                System.out.println("Main thread interrupted.");
             }
-            System.out.println(getName() + " terminating.");
         }
+        System.out.println("Main thread ending.");
+    }
+
+    //Can be replaced with a static factory method createAndStart()
+    public static MyExtendThread createAndStart(String name) {
+        MyExtendThread myThrd = new MyExtendThread(name);
+        myThrd.start();
+        return myThrd;
+    }
+
+    /*
+    To try createAndStart replace
+    System.out.println("Main thread starting.");
+    MyExtendThread mt = new MyExtendThread(Child #1)
+    with
+    MyExtendThread mt = MyExtendThread.createAndStart(Child #1)
+     */
+}
+
+class MyExtendThread extends Thread {
+    // Construct a new thread.
+    MyExtendThread(String name) {
+        //name thread. Super is used to call this version of Thread's constructor:
+        //Thread(String threadName)
+        super(name);
+        //Thread provides the ability to hold a thread's name.
+        //Thus, no instance variable is required by MyExtendThread to store the name
+
+    }
+
+    // Entry point of Thread
+    public void run() {
+        System.out.println(getName() + " starting.");
+        try {
+            for (int count = 0; count < 10; count++) {
+                Thread.sleep(400);
+                System.out.println("In " + getName() + ", count is " + count);
+            }
+        } catch (InterruptedException exc) {
+            System.out.println(getName() + " interrupted");
+        }
+        System.out.println(getName() + " terminating.");
     }
 }
