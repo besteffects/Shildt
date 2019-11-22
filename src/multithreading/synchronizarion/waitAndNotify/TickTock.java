b9baff8969042e29d3d639fa684b2f7bce1593da
besteffects <besteffects@gmail.com>
2019-11-22 01:38:33 -0500

    Tick-tock app. few fixes

--- src/multithreading/synchronizarion/waitAndNotify/TickTock.java
+++ src/multithreading/synchronizarion/waitAndNotify/TickTock.java
@@ -1,9 +1,8 @@
 package multithreading.synchronizarion.waitAndNotify;
 
-import multithreading.MyThreadJoin;
-
 // Use wait() and notify() to create a ticking clock
 class TickTock {
+
     String state; // contains the state of the clock
 
     synchronized void tick(boolean running) {
@@ -20,7 +19,7 @@ class TickTock {
         notify(); //let tock() run
         try {
             while (!state.equals("tocked"))
-                wait();//
+                wait();// tick waits for tock to complete
         } catch (InterruptedException exc) {
             System.out.println("Thread interrupted.");
         }
@@ -30,48 +29,68 @@ class TickTock {
         if (!running) { //stop the clock
             state = "tocked";
             notify(); //notify any waiting threads
+            return;
+        }
 
-            System.out.println("Tock");
+        System.out.println("Tock");
 
-            state = "tocked"; //set the current state to tocked
+        state = "tocked"; //set the current state to tocked
 
-            notify(); //let tick() run //tock() notifies tick()
-            try {
-                while (!state.equals("ticket")) {
-                    wait(); //wait for tick to complete (tock() waits for tick() )
-                }
-            } catch (InterruptedException exc) {
-                System.out.println("Thread interrupted.");
+        notify(); //let tick() run //tock() notifies tick()
+        try {
+            while (!state.equals("ticked")) {
+                wait(); //wait for tick to complete (tock() waits for tick() )
             }
+        } catch (InterruptedException exc) {
+            System.out.println("Thread interrupted.");
         }
     }
 }
 
-class Mythread implements Runnable{
+class MyThread1 implements Runnable {
     Thread thrd;
     TickTock ttOb;
 
     //Construct a new thread.
-    Mythread(String name, TickTock tt){
-        thrd = new Thread(this,name);
-        ttOb=tt;
+    MyThread1(String name, TickTock tt) {
+        thrd = new Thread(this, name);
+        ttOb = tt;
     }
 
-    // A factory method that creates and starts a thread.
-    public static Mythread createAndStart(String name, TickTock tt){
-        Mythread myThrd = new Mythread(name,tt);
+    //  A factory method that creates and starts a thread.
+    static MyThread1 createAndStart(String name, TickTock tt) {
+        MyThread1 myThrd = new MyThread1(name, tt);
+
         myThrd.thrd.start(); //start the thread
         return myThrd;
     }
 
+
     //Entry point of thread.
-    public void run(){
-        if (thrd.getName().compareTo("Tick")==0){
-            for (int i = 0; i <5 ; i++) {
+    public void run() {
+        if (thrd.getName().compareTo("Tick") == 0) {
+            for (int i = 0; i < 5; i++) {
                 ttOb.tick(false);
             }
-        } else for (int i = 0; i <5 ; i++) {
-            ttOb.tock(true);
+        } else {
+            for (int i = 0; i < 5; i++) ttOb.tock(true);
+                ttOb.tock(false);
         }
     }
 }
+
+/*class ThreadCom {
+    public static void main(String[] args) {
+        TickTock tt = new TickTock();
+        MyThread1 mt1 = new MyThread1.createAndStart("Tick", tt);
+        MyThread1 mt2 = new MyThread1.createAndStart("Tock", tt);
+        try {
+            mt1.thrd.join();
+            mt2.thrd.join();
+        } catch (InterruptedException exc) {
+            System.out.println("Main thread interrupted");
+        }
+    }
+}
+
+ */
