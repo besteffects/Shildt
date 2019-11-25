a627d70e9e4ac18cef6a9833e33f70d8f7cba73d
besteffects <besteffects@gmail.com>
2019-11-24 23:32:27 -0500

    Use of values() and valueOf() for enums

---
+++ src/chapter12/enumeration/valueOf/Transport.java
@@ -0,0 +1,27 @@
+package chapter12.enumeration.valueOf;
+
+// Use built-in enumeration methods
+// An enumeration of Transport varieties
+enum Transport {
+    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+}
+
+class EnumDemo2 {
+    public static void main(String[] args) {
+        Transport tp;
+        System.out.println("Here are all transport constants");
+
+        //use values()
+        Transport allTransports[] = Transport.values(); //Obtain an array of Transport constants
+        for (Transport t : allTransports) {
+            System.out.println(t);
+        }
+
+        System.out.println();
+
+        //use valueOf()
+        tp = Transport.valueOf("AIRPLANE"); //Obtain the constant with the name AIRPLANE
+        System.out.println("tp contains " + tp);
+
+    }
+}
