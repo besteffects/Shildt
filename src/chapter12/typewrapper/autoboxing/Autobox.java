97589742bcac5a4f262dd809f6832d261f0f086e
besteffects <besteffects@gmail.com>
2019-11-27 17:17:54 -0500

    Autoboxing/unboxing examples

--- src/chapter12/typewrapper/autoboxing/Autobox.java
+++ src/chapter12/typewrapper/autoboxing/Autobox.java
@@ -1,4 +1,10 @@
 package chapter12.typewrapper.autoboxing;
 
+//Demonstrate autoboxing/unboxing
 public class Autobox {
+    public static void main(String[] args) {
+        Integer iOb = 100; //autobox and int
+        int i = iOb; //auto-unbox
+        System.out.println(i + " " + iOb);
+    }
 }
