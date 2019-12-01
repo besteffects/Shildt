566cbb0a91055e0c94617b1b8bfd355e102c9fa3
besteffects <besteffects@gmail.com>
2019-11-30 23:12:12 -0500

    Bounded generic types. Part2

--- src/chapter13/generics/boundedTypes/NumericFns.java
+++ src/chapter13/generics/boundedTypes/NumericFns.java
@@ -23,3 +23,22 @@ class NumericFns<T extends Number> {
     // ...
 }
 
+//Demonstrate NumericFNS
+class BoundsDemo {
+    public static void main(String[] args) {
+        NumericFns<Integer> iOb = new NumericFns<Integer>(5); //Integer is OK because it is a subclass by Number
+        System.out.println("Reciprocal of iOb is " + iOb.reciprocal());
+
+        System.out.println("Fractional component of iOb is " +
+                iOb.fraction());
+
+        System.out.println();
+
+        NumericFns<Double> dOb =
+                new NumericFns<Double>(5.25);
+        System.out.println("Reciprocal of dOb is " +
+                dOb.reciprocal());
+        System.out.println("Fractional component of dOb is " +
+                dOb.fraction());
+    }
+}
