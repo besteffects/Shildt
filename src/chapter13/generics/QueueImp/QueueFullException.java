84d78e2501dfd19e9db4034d709eeeb8d372bbe6
besteffects <besteffects@gmail.com>
2019-12-05 03:22:32 -0500

    GenQueue

--- src/chapter13/generics/QueueImp/QueueFullException.java
+++ src/chapter13/generics/QueueImp/QueueFullException.java
@@ -1,13 +1,15 @@
 package chapter13.generics.QueueImp;
+
 // An exception for full queue-full errors
-class QueueFullException extends Exception{
+class QueueFullException extends Exception {
     int size;
-    QueueFullException (int s){
-        size=s;
+
+    QueueFullException(int s) {
+        size = s;
     }
 
-    public String toString(){
-        return "\nQueue is full.Maximum size is " +size;
+    public String toString() {
+        return "\nQueue is full.Maximum size is " + size;
     }
 }
 
