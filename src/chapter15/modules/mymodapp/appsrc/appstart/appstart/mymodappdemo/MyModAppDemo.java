599a3c5a0801745212a6d43b52d72784c1a345b0
besteffects <besteffects@gmail.com>
2019-12-11 00:24:01 -0500

    Added MyModAppDemo

---
+++ src/chapter15/modules/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,13 @@
+package chapter15.modules.mymodapp.appsrc.appstart.appstart.mymodappdemo;
+import chapter15.modules.mymodapp.appsrc.appfuncs.appfuncs.simplefuncs.SimpleMathFuncs;
+public class MyModAppDemo {
+    public static void main(String[] args) {
+        if (SimpleMathFuncs.isFactor(2,10)){
+            System.out.println("2 is a factor of 10");
+        }
+
+        System.out.println("Smallest factor common to both 35 and 105 is " + SimpleMathFuncs.lcf(35,105));
+
+        System.out.println("Largest factor common to both 35 and 105 is " + SimpleMathFuncs.gcf(36,105));
+    }
+}
